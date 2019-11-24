0c0afc07fbb2a2956a487d755e6223e123965fd1
Guilherme Oliveira <guizera11@hotmail.com.br>
2019-11-24 07:23:18 -0300

    atualizacao calendario rotina

--- Gato/src/gato/CalendarioRotina.java
+++ Gato/src/gato/CalendarioRotina.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CalendarioRotina;
+package gato;
 
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
--- Gato/src/gato/Rotina.java
+++ Gato/src/gato/Rotina.java
@@ -3,7 +3,18 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package CalendarioRotina;
+package gato;
+
+import java.awt.HeadlessException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 /**Classe que cria um objeto do tipo Calendario De Rotina.
  *@
@@ -16,7 +27,56 @@ public class Rotina {
         int[][] duracao = new int[25][8];
         String[][] infos = new String[25][8];
         int[] quantidade_eventos = new int[8];
-        String[][] horarios = new String[25][8];
+        String[][] horarios = new String[25][8];//armazena a partir do 1 todos menos hora (00:00 = cod 0)
+        
+        String url = "jdbc:mysql://localhost/gato?useSSL=false", usuario = "root", senha = "root";
+        Connection conexao;
+        Statement stm;
+        ResultSet rs;
+        Integer qtdEventos;
+        int i;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Rotina.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        try {
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            
+            
+            //comeco de capturar informacoes de domingo
+            stm = conexao.createStatement();
+            rs = stm.executeQuery("select titulo, hora_inicio, hour(timediff(hora_fim, hora_inicio)) as duracao from evento_semanal where dia_semana = 'Domingo' order by hora_inicio asc");
+            
+            i = 0;
+            while (rs.next()) {
+                infos[i][1] = rs.getString("titulo");
+                duracao[i][1] = rs.getInt("duracao");
+                horarios[i][1] = rs.getString("hora_inicio");  
+                i++;
+            }
+            stm.close();
+            //fim de capturar informacoes de domingo
+            
+            //comeco de contar quantidade de eventos no domingo
+            stm = conexao.createStatement();
+            
+            rs = stm.executeQuery("select count(pk_evento_semanal) as qtd_eventos from evento_semanal where dia_semana = 'Domingo'");
+            quantidade_eventos[1] = rs.getInt("qtd_eventos");
+            
+            stm.close();
+            conexao.close();
+            //fim de contar quantidade de eventos no domingo
+        } catch (HeadlessException | SQLException excp) {
+            JOptionPane.showMessageDialog(null, "Erro ao carregar do banco.");
+            System.err.println(excp);
+        }
+        
+        
+        
+        //comando que retorna a diferenca entre duas horas do mysql
+        //select hour(timediff(hora_fim, hora_inicio)) from Evento_Semanal order by dia_semana asc, hora_inicio asc; 	
         
         //Os dias da semana comecam no domingo com codigo 1 e vao ate sabado codigo 7;
         //Os codigos do horario comecam da meia-noite "00:00" com codigo 0 até "24:00" com codigo 24;
@@ -24,17 +84,17 @@ public class Rotina {
         //o mesmo é válido para os outros parametros da classe (duracao, infos e horarios), somente quantidade de eventos que
         // precisa apenas do codigo do dia da semana.
         
-        //duracao[12][4] = 3;
-        //duracao[11][6] = 4;
+        duracao[12][4] = 3;
+        duracao[11][6] = 4;
         
-        //infos[12][4] = "Almoco";
-        //infos[11][6] = "Aula de LPP";
+        infos[12][4] = "Almoco";
+        infos[11][6] = "Aula de LPP";
         
-        //quantidade_eventos[4] = 1;
-        //quantidade_eventos[6] = 1;
+        quantidade_eventos[4] = 1;
+        quantidade_eventos[6] = 1;
         
-        //horarios[12][4] = "12:00";
-        //horarios[11][6] = "11:00";
+        horarios[12][4] = "12:00";
+        horarios[11][6] = "11:00";
         
         cr = new CalendarioRotina();
         cr.setDuracao(duracao);
